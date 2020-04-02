2bb6db8499480d8a4638b1547ecbb8a070b25a74
Jerry Chen <jerrychen017@gmail.com>
2020-04-01 22:58:00 -0400

    fix socket_addr_len

--- interactive_server/interactive_server.c
+++ interactive_server/interactive_server.c
@@ -101,7 +101,7 @@ int main(int argc, char **argv)
                     for (int i = 0; i < num_users; i++)
                     {
                         sendto(sk, &echo, sizeof(echo), 0,
-                               (struct sockaddr *)&users[i].socket_addr, sizeof(users[i].socket_addr_len));
+                               (struct sockaddr *)&users[i].socket_addr, users[i].socket_addr_len);
                         printf("sent an echo packet with name <%s> and id <%d>\n", users[i].name, users[i].id);
                     
                     }
