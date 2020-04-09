9a106fbbd4e487076e5fb0db6a1fe159bf893789
Dan Qian <daniel.j.qian@gmail.com>
2020-04-09 12:00:27 -0400

    logger

---
+++ bandwidth_measurement/logger.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "logger.h"
+
+void sendMessage(char* str, int n)
+{
+    printf("%s\n", str);
+}
---
+++ bandwidth_measurement/logger.h
@@ -0,0 +1,9 @@
+//
+// Created by Jerry Chen on 3/25/20.
+//
+
+#ifndef LOGGER_H
+#define LOGGER_H
+
+void sendMessage(char* str, int n);
+#endif //UDP_TOOLS_LOGGER_H
