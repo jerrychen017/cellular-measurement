8fe0c691c3441c63406ffc95f3b8278f4a9b654b
Dan Qian <daniel.j.qian@gmail.com>
2020-04-16 12:27:25 -0400

    delete old server

--- echo_client/echo_client.h
+++
@@ -1,8 +0,0 @@
-#ifndef ECHO_CLIENT_H
-#define ECHO_CLIENT_H
-#include "../interactive_server/net_include.h"
-#include "../bandwidth_measurement/bandwidth_utils.h"
-
-int client_bind(const char* address, int port);
-char * echo_send(const char* address, int port, int seq);
-#endif //UDP_TOOLS_INTERACTIVE_CLIENT_H
