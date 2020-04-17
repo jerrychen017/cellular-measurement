2aa322dd6d8fc53c05ca84a8bf3506fa504eff80
Dan Qian <daniel.j.qian@gmail.com>
2020-04-17 00:17:27 -0400

    fix server using rates with old packets

--- bandwidth_measurement/mergedServer.c
+++ bandwidth_measurement/mergedServer.c
@@ -58,8 +58,12 @@ void receive(int s_bw, int s_inter, int predMode, int max_num_users)
     int len;
 
     // state variables
-    // Current rate the server is receiving at, caclulated according to predMode
-    double rate = START_SPEED;
+    // Current rate the server expectes to receive at
+    double curRate = 0;
+    // Rate that ewma predicts
+    double ewmaRate = 0;
+    // only use measurements after we have received WINDOW_SIZE packets at current rate
+    int numAtCurRate = 0;
 
     // next expected seq number
     int seq = 0;
@@ -75,9 +79,11 @@ void receive(int s_bw, int s_inter, int predMode, int max_num_users)
     struct timeval barrivals[BURST_SIZE];
     int breceived[BURST_SIZE];
 
+
+
     memset(received, 0, sizeof(received));
     memset(breceived, 0, sizeof(breceived));
-    
+
 
     // variables to store caclulations in
     double calculated_speed;
@@ -103,7 +109,7 @@ void receive(int s_bw, int s_inter, int predMode, int max_num_users)
         if (num > 0) {
             if (FD_ISSET(s_bw, &read_mask)) {
                 len = recvfrom(s_bw, &data_pkt, sizeof(data_packet), 0,
-                        (struct sockaddr *) &from_addr, &from_len);
+                               (struct sockaddr *) &from_addr, &from_len);
                 if (len < 0) {
                     perror("socket error");
                     exit(1);
@@ -124,12 +130,19 @@ void receive(int s_bw, int s_inter, int predMode, int max_num_users)
                     ack_pkt.seq_num = 0;
 
                     sendto_dbg(s_bw, &ack_pkt, sizeof(ack_pkt), 0,
-                            (struct sockaddr *) &from_addr, from_len);
+                               (struct sockaddr *) &from_addr, from_len);
                 }
 
 
                 double expectedRate = data_pkt.hdr.rate;
                 int currSeq = data_pkt.hdr.seq_num;
+
+                // keep track of how many packets we've received at the current rate
+                if (expectedRate != curRate) {
+                    curRate = expectedRate;
+                    numAtCurRate = 0;
+                }
+                numAtCurRate++;
 
                 // out of order packet, drop
                 if (currSeq < seq) {
@@ -145,7 +158,7 @@ void receive(int s_bw, int s_inter, int predMode, int max_num_users)
                 int i = currSeq % BURST_SIZE;
                 received[i] = 1;
                 gettimeofday(&arrivals[i], NULL);
-                
+
                 if (data_pkt.hdr.type == NETWORK_BURST){
                     // first burst packet we have received
                     if (burstSeq == 0) {
@@ -169,9 +182,8 @@ void receive(int s_bw, int s_inter, int predMode, int max_num_users)
                         if (burstSeq - 1 != bFirst) {
                             calculated_speed = interval_to_speed(tm_diff, (burstSeq - 1) - bFirst);
                             printf("Burst calculated speed of %.4f Mbps\n", calculated_speed);
-                            rate = calculated_speed;
                             report_pkt.type = NETWORK_REPORT;
-                            report_pkt.rate = rate;
+                            report_pkt.rate = calculated_speed;
                             report_pkt.seq_num = 0;
 
                             sendto_dbg(s_bw, &report_pkt, sizeof(report_pkt), 0,
@@ -180,39 +192,43 @@ void receive(int s_bw, int s_inter, int predMode, int max_num_users)
                         }
                     }
 
+                    // First assume rate is as expected (if we are unable to calculate because we
+                    // haven't received enough, we don't want to do anything)
+                    double calcRate = expectedRate;
 
                     // EWMA
-                    if (predMode == 0 && (currSeq % INTERVAL_SIZE) >= 2) {
+                    if (predMode == 0 && numAtCurRate >= 2) {
                         if (!received[(currSeq - 1) % BURST_SIZE]) continue;
 
-                        tm_diff = diffTime(arrivals[currSeq % BURST_SIZE], arrivals[(currSeq - 1) % BURST_SIZE]);   
+                        tm_diff = diffTime(arrivals[currSeq % BURST_SIZE], arrivals[(currSeq - 1) % BURST_SIZE]);
                         calculated_speed = interval_to_speed(tm_diff, 1);
-                        rate = (ALPHA * calculated_speed) + (1 - ALPHA) * rate;   
-                        printf("Computed sending rate of %.4f Mbps\n", rate);
+                        ewmaRate = (ALPHA * calculated_speed) + (1 - ALPHA) * ewmaRate;
+                        printf("Computed sending rate of %.4f Mbps\n", ewmaRate);
+                        calcRate = ewmaRate;
 
                     }
                     // Running Avg
-                    if (predMode == 1 && (currSeq % INTERVAL_SIZE) >= BURST_SIZE) {
+                    if (predMode == 1 && numAtCurRate >= BURST_SIZE) {
                         if (!received[(currSeq + 1) % BURST_SIZE]) continue;
 
                         // Wrap around average of packets
-                        tm_diff = diffTime(arrivals[currSeq % BURST_SIZE], arrivals[(currSeq + 1) % BURST_SIZE]);   
+                        tm_diff = diffTime(arrivals[currSeq % BURST_SIZE], arrivals[(currSeq + 1) % BURST_SIZE]);
                         calculated_speed = interval_to_speed(tm_diff, BURST_SIZE - 1);
-                        rate = calculated_speed; //Figure out threshold
-                        printf("Computed sending rate of %.4f Mbps\n", rate);
+                        calcRate = calculated_speed; //Figure out threshold
+                        printf("Computed sending rate of %.4f Mbps\n", calcRate);
                     }
 
                     // Send report packet if we are under 90 percent of expected rate
-                    if (rate <= THRESHOLD*expectedRate) {
+                    if (calcRate <= THRESHOLD * expectedRate) {
                         report_pkt.type = NETWORK_REPORT;
-                        report_pkt.rate = rate;
+                        report_pkt.rate = calcRate;
                         report_pkt.seq_num = 0;
                         sendto_dbg(s_bw, &report_pkt, sizeof(report_pkt), 0,
-                            (struct sockaddr *) &from_addr, from_len);
-                        printf("Computed rate %.4f below threshold, actual rate %.4f\n", rate, expectedRate);
+                                   (struct sockaddr *) &from_addr, from_len);
+                        printf("Computed rate %.4f below threshold, actual rate %.4f\n", calcRate, expectedRate);
                     }
                 }
-               
+
                 // increment seq
                 seq = currSeq + 1;
             }
