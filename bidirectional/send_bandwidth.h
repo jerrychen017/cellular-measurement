bfd941f0cc073aada555de3838b906d1a3d48289
Jerry Chen <jerrychen017@gmail.com>
2020-04-20 16:26:07 -0400

    remove data_generator main

--- bidirectional/send_bandwidth.h
+++ bidirectional/send_bandwidth.h
@@ -1,6 +1,10 @@
+#ifndef SEND_BANDWIDTH
+#define SEND_BANDWIDTH
 #include "net_include.h"
 struct send_bandwidth_args {
     struct sockaddr_in addr; 
     int port; 
 };
 void send_bandwidth(void * arg);
+
+#endif
