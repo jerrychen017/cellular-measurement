9283b41eff21fb0a8503fbb6b4be960b7e6cd20e
Jerry Chen <jerrychen017@gmail.com>
2020-04-20 14:40:49 -0400

    unmerge server

---
+++ interactive_server/interactive_client.h
@@ -0,0 +1,24 @@
+#ifndef INTERACTIVE_CLIENT_H
+#define INTERACTIVE_CLIENT_H
+#include "net_include.h"
+#include "../bandwidth_measurement/bandwidth_utils.h"
+
+int interactive_connect(const char name[NAME_LENGTH]);
+
+/**
+ * send an interactive packet to the server and returns the sequence number
+ * @param x x_coordinate
+ * @param y y_coordinate
+ * @return status code
+ */
+int send_interactive_packet(int seq_num, float x, float y);
+
+/**
+ * receive an interactive packet with a certain sequence number
+ * @return InteractivePacket
+ */
+InteractivePacket receive_interactive_packet();
+
+void init_socket(const char *address, int port);
+
+#endif //UDP_TOOLS_ECHO_CLIENT_H
