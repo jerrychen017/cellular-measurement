a8da61776b88950c8b1a04964781271f567ca3e3
Guangrui Chen <gchen41@jhu.edu>
2020-04-23 18:36:07 -0400

    remove old bandwidth

--- bandwidth_measurement/net_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef NET_UTILS_H
-#define NET_UTILS_H
-#include "net_include.h"
-struct sockaddr_un get_datagen_addr(bool android, socklen_t *len);
-struct sockaddr_un get_controller_addr(bool android, socklen_t *len);
-int setup_unix_socket(struct sockaddr_un addr, socklen_t len);
-
-struct sockaddr_in addrbyname(const char *hostname, int port);
-#endif
